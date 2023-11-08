b5eca119e5c865831926b86f89b67bc029fc4c45
Elfarodelbosque <elfarodelbosquemadrid@gmail.com>
2023-11-08 16:23:26 +0100

    actualizacion

--- js/slider.js
+++ js/slider.js
@@ -35,3 +35,37 @@ punto.forEach( ( cadaPunto , i )=> {
 })
 
 
+
+const grande2    = document.querySelector('.grande2')
+const punto2     = document.querySelectorAll('.punto2')
+
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+// Recorrer TODOS los punto
+punto2.forEach( ( cadaPunto2 , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto2[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -50
+
+        // MOVEMOS el grand
+        grande2.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto2.forEach( ( cadaPunto2 , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto2[i].classList.remove('activo2')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto2[i].classList.add('activo2')
+
+    })
+})
+
